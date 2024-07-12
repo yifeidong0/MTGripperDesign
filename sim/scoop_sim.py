2c07000b68b03c2a12bd66f15a5f299ddafc1682
YvesDong <602522935@qq.com>
2024-07-12 23:12:54 +0200

    scoop sim updated with bread/home/yif/miniconda3/envs/mtbo/bin/python /home/yif/Documents/KTH/git/MTGripperDesign/test/scoop_test.py

--- sim/scoop_sim.py
+++ sim/scoop_sim.py
@@ -11,10 +11,11 @@ from utils.generate_scoop import generate_scoop
 from utils.vhacd import decompose_mesh
 
 class ScoopingSimulation:
-    def __init__(self, object_type='insole', coef=[1, 1], use_gui=True):
+    def __init__(self, object_type='bread', coef=[1, 1], use_gui=True):
         self.coef = coef
         self.object_type = object_type
         self.use_gui = use_gui
+        self.g = -9.81
 
         # Connect to PyBullet
         if self.use_gui:
@@ -26,7 +27,7 @@ class ScoopingSimulation:
         p.resetSimulation(p.RESET_USE_DEFORMABLE_WORLD)
         p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
         p.setRealTimeSimulation(0)
-        p.setGravity(0, 0, -9.81)
+        p.setGravity(0, 0, self.g)
         self.time_step = 1.0 / 240.0
         self.plane_id = p.loadURDF("plane.urdf", basePosition=[0, 0, 0])
 
@@ -43,6 +44,9 @@ class ScoopingSimulation:
         self.goal_radius = 0.5
         self.goal_position = [2.5, 4.5] # workspace [[0,5], [0,5]]
         self.tex = p.loadTexture("uvmap.png")
+        self.hover_height = 1.
+        self.reached_height = 0.8 * self.hover_height
+        self.unsafe_height = 0.7 * self.hover_height
 
         self.load_obstacle()
         self.setup(reset_task_and_design=True)
@@ -54,26 +58,24 @@ class ScoopingSimulation:
                 reset_pose (bool): Whether to reset the object and robot poses (for a new episode).
         """
         # Set the object and robot poses
-        if self.object_type == 'insole':
-            self.object_orientation = p.getQuaternionFromEuler([0, 0, random.normalvariate(0, 0.1)])
-            self.object_position = [
-                                    random.normalvariate(2, .3,), 
+        if self.object_type == 'bread':
+            self.object_orientation = p.getQuaternionFromEuler([math.pi/2, 0, math.pi/2+random.normalvariate(0, 0.1)])
+            self.object_position = [random.normalvariate(2, .3,), 
                                     random.normalvariate(2.1, .3,), 
                                     self.body_height]
         elif self.object_type == 'pillow':
             self.object_orientation = p.getQuaternionFromEuler([0, 0, random.normalvariate(0, 0.1)]) 
-            self.object_position = [
-                                    random.normalvariate(3, .3,), 
+            self.object_position = [random.normalvariate(3, .3,), 
                                     random.normalvariate(2.5, .3,), 
                                     self.body_height]
         self.robot_position = [random.normalvariate(0.5, .1),
-                                random.normalvariate(2.5, .3),
-                                0]
+                               random.normalvariate(2.5, .3),
+                               0]
         self.robot_orientation = p.getQuaternionFromEuler([0,0,0])
 
         # Create the object and robot
         if reset_task_and_design:
-            if self.object_type == 'insole':
+            if self.object_type == 'bread':
                 self.load_insole()
             elif self.object_type == 'pillow':
                 self.load_pillow()
@@ -147,25 +149,25 @@ class ScoopingSimulation:
             urdf_file.write(urdf_template)
 
         self.robot_id = p.loadURDF(f"asset/poly_scoop/{unique_urdf_filename}", basePosition=self.robot_position, baseOrientation=self.robot_orientation)
-        # self.robot_id = p.loadURDF(f"asset/scoop.urdf", basePosition=self.robot_position, baseOrientation=self.robot_orientation)
+        # self.robot_id = p.loadURDF(f"asset/scoop_1.00_1.00.urdf", basePosition=self.robot_position, baseOrientation=self.robot_orientation)
 
         # Load urdf
         p.changeVisualShape(self.robot_id, -1, rgbaColor=[0, 0, 1, 1])
-        p.changeDynamics(self.robot_id, -1, lateralFriction=0.6, spinningFriction=0.4, rollingFriction=0.4)
+        p.changeDynamics(self.robot_id, -1, lateralFriction=0.1, spinningFriction=0.1, rollingFriction=0.1)
 
     def load_insole(self):
         self.object_id = p.loadSoftBody(
-            "asset/insole.vtk",
+            "asset/bread.vtk",
             basePosition=self.object_position,
             baseOrientation=self.object_orientation,
-            scale=4,
+            scale=.5,
             mass=self.object_mass,
             useNeoHookean=1,
             NeoHookeanMu=1.5,
             NeoHookeanLambda=1,
             NeoHookeanDamping=0.5,
             useSelfCollision=1,
-            frictionCoeff=0.5,
+            frictionCoeff=0.1,
             collisionMargin=0.001
         )
         p.changeVisualShape(self.object_id, -1, rgbaColor=[1, 1, 1, 1], textureUniqueId=self.tex, flags=0)
@@ -182,13 +184,13 @@ class ScoopingSimulation:
             NeoHookeanLambda=1,
             NeoHookeanDamping=0.5,
             useSelfCollision=1,
-            frictionCoeff=0.5,
+            frictionCoeff=0.1,
             collisionMargin=0.001
         )
         p.changeVisualShape(self.object_id, -1, rgbaColor=[1, 1, 1, 1], textureUniqueId=self.tex, flags=0)
 
     def load_obstacle(self):
-        # Create a heavy box to fix the insole at the other end
+        # Create a heavy box to fix the bread at the other end
         box = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.1, 1.8, 0.1])
         self.obstacle = p.createMultiBody(
             baseMass=0,
@@ -197,9 +199,21 @@ class ScoopingSimulation:
             baseOrientation=p.getQuaternionFromEuler([0, 0, 0])
         )
         
-    def eval_robustness(self, slack=0.1):
-        robustness = 0
-        return robustness
+    def eval_robustness(self, dt=1./240., acc_lim=1000):
+        rand_acc = [random.uniform(-acc_lim, acc_lim) for _ in range(3)]
+        total_acc = rand_acc
+        total_acc[2] = self.g
+        delta_vel = [a * dt for a in total_acc]
+        curr_vel = p.getBaseVelocity(self.object_id)[0]
+        new_vel = [v + dv for v, dv in zip(curr_vel, delta_vel)]
+        p.resetBaseVelocity(self.object_id, new_vel, [0, 0, 0])
+
+        if self.object_position[2] > self.unsafe_height:
+            self.steps_on_the_scoop += 1
+        else:
+            self.done_evaluation = True
+        
+        return self.done_evaluation
     
     def run(self, num_episodes=1):
         avg_score = 0
@@ -211,75 +225,70 @@ class ScoopingSimulation:
         avg_score /= num_episodes
         return avg_score
 
-    def run_onetime(self, rob_eval_freq=10):
+    def run_onetime(self,):
         i = 0
-        avg_robustness = 0
         target_reached = False
+        pause_and_check = False
+        self.steps_on_the_scoop = 0
+        self.done_evaluation = False
+
         while True:
-            if i % 100 == 0:
+            if i % 300 == 0:
                 print(f"Step {i}")
 
             # Get current position of the object and robot
             self.object_position = np.array(p.getBasePositionAndOrientation(self.object_id)[0])
             self.robot_position = np.array(p.getBasePositionAndOrientation(self.robot_id)[0])
 
-            # # Check if the object is in the goal region
-            # distance_to_goal = np.linalg.norm(self.object_position[:2] - np.array(self.goal_position))
-            # if distance_to_goal < self.goal_radius: # TODO: height check
-            #     target_reached = True
-            #     break
-
             # Apply velocity on scoop
-            if i > 100 and i < 1000:
+            if i < 100:
+                pass
+            elif i >= 100 and i < 500:
                 p.applyExternalForce(self.robot_id, -1, [7, 0, 0], self.robot_position, p.WORLD_FRAME)
             
-            # Lift the scoop
-            if i > 1000 and i < 1300:
-                p.applyExternalForce(self.robot_id, -1, [0, 0, 11], self.robot_position, p.WORLD_FRAME)
-
-            # Fix the scoop in the air
-            if i > 1300:
-                p.changeDynamics(self.robot_id, -1, mass=0)
-                p.resetBaseVelocity(self.robot_id, [0, 0, 0], [0, 0, 0])
+            elif i >= 500:
+                if self.robot_position[2] < self.hover_height: # Lift the scoop
+                    p.applyExternalForce(self.robot_id, -1, [0, 0, 11], self.robot_position, p.WORLD_FRAME)
+                elif not pause_and_check: # Fix the scoop in the air
+                    p.changeDynamics(self.robot_id, -1, mass=0)
+                    p.resetBaseVelocity(self.robot_id, [0, 0, 0], [0, 0, 0])
+                    target_reached = True if self.object_position[2] > self.reached_height else False
+                    pause_and_check = True
+                elif not self.done_evaluation:
+                    self.eval_robustness()
 
             p.stepSimulation()
-            time.sleep(self.time_step)
+            if self.use_gui:
+                time.sleep(self.time_step)
             i += 1
 
-            # Evaluate robustness
-            if i % rob_eval_freq == 0:
-                rob = self.eval_robustness(slack=1)
-                avg_robustness += rob
-
-            # Break condition for the simulation
-            if (i >= 1500 
-                or (self.robot_position[0]>5 or self.robot_position[0]<0 or self.robot_position[1]>6 or self.robot_position[1]<0)
-                or (self.object_position[0]>5 or self.object_position[0]<0 or self.object_position[1]>6 or self.object_position[1]<0)
-                ):
+            # Break if out of bounds or done evaluation
+            if ((self.robot_position[0]>6 or self.robot_position[0]<0 or self.robot_position[1]>6 or self.robot_position[1]<0)
+                or (self.object_position[0]>6 or self.object_position[0]<0 or self.object_position[1]>6 or self.object_position[1]<0)
+                or self.done_evaluation):
                 break
 
             # Get camera image (time consuming, only for visualization purpose)
             # p.getCameraImage(320, 320, viewMatrix=self.viewMatrix, projectionMatrix=self.projectionMatrix)
 
         # Final score calculation
-        avg_robustness = 0 if i == 0 else avg_robustness / (i // rob_eval_freq)
         final_score = 1.0 if target_reached else 0.0
-        final_score += avg_robustness
+        final_score += self.steps_on_the_scoop / 10000.0
 
         return final_score
 
 if __name__ == "__main__":
     coef = [1, 1]
-    simulation = ScoopingSimulation('pillow', coef=coef, use_gui=True)  # insole or pillow
+    simulation = ScoopingSimulation('bread', coef=coef, use_gui=1)  # bread or pillow
     for i in range(3):
         print('Iteration %d' % i)
         final_score = simulation.run(1)
         print("Final Score:", final_score)
 
-        # randomly select insole or pillow and new design parameters
+        # randomly select bread or pillow and new design parameters
         coef = [random.uniform(.5, 5), 
                 random.uniform(0.2,1.3), ]
         if random.random() < 0.5:
-            simulation.reset_task_and_design('insole', coef,)
+            simulation.reset_task_and_design('bread', coef,)
         else:
             simulation.reset_task_and_design('pillow', coef,)
--- test/scoop_test.py
+++ test/scoop_test.py
@@ -19,73 +19,72 @@ p.setGravity(0, 0, g)
 planeUid = p.loadURDF("plane.urdf", basePosition=[0, 0, 0])
 
 # Create scoop
-coef = [random.uniform(.5, 5), 
-        random.uniform(0,1.5), 
-        random.uniform(0,1.5)]
-coef = [1, 1]
-unique_obj_filename = 'test.obj'
-generate_scoop(coef, f"asset/{unique_obj_filename}")
+# coef = [random.uniform(.5, 5), 
+#         random.uniform(0,1.5), 
+#         random.uniform(0,1.5)]
+# coef = [1, 1]
+# unique_obj_filename = 'test.obj'
+# generate_scoop(coef, f"asset/{unique_obj_filename}")
 
-# Decompose the mesh
-decompose_mesh(pb_connected=True, input_file=f"asset/{unique_obj_filename}", resolution = int(1e2),)
+# # Decompose the mesh
+# decompose_mesh(pb_connected=True, input_file=f"asset/{unique_obj_filename}", resolution = int(1e2),)
 
 # Load scoop as robot end-effector
 mass_scoop = 1
-# robot_orientation = p.getQuaternionFromEuler([math.pi/2, -.6-math.pi/2, 0*math.pi/2])
 robot_orientation = p.getQuaternionFromEuler([0,0,0])
 scoop = p.loadURDF(
-    "asset/scoop.urdf", 
+    "asset/scoop_1.00_1.00.urdf", 
     basePosition=[1,2.5,0],  # Position above the ground
     baseOrientation=robot_orientation,
     globalScaling=1
 )
-p.changeDynamics(scoop, -1, lateralFriction=0.6, spinningFriction=0.4, rollingFriction=0.4)
+p.changeDynamics(scoop, -1, lateralFriction=0.0, spinningFriction=0.4, rollingFriction=0.4)
 
 # Change mass
 p.changeDynamics(scoop, -1, mass=mass_scoop)
 
-# Load the insole soft body
+# Load the bread soft body
 tex = p.loadTexture("uvmap.png")
 mass_pillow = 0.005
-# insole = p.loadSoftBody(
-#     "asset/insole.vtk", # pillow, insole
-#     basePosition=[2.5, 2.5, 1.2],  # Position on top of the scoop
-#     baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
-#     scale=5,
-#     mass=0.005,
-#     useNeoHookean=1,
-#     NeoHookeanMu=1.5,
-#     NeoHookeanLambda=1,
-#     NeoHookeanDamping=0.1,
-#     useSelfCollision=1,
-#     frictionCoeff=0.5,
-#     collisionMargin=0.001
-# )
-# p.changeVisualShape(insole, -1, rgbaColor=[1, 1, 1, 1], textureUniqueId=tex, flags=0)
-
-# Load the pillow soft body
-pillow = p.loadSoftBody(
-    "asset/pillow.vtk", # pillow, insole
-    basePosition=[2.5, 2.5, 1],  # Position on top of the scoop
-    baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
-    scale=2.5,
-    mass=mass_pillow,
+object_id = p.loadSoftBody(
+    "asset/bread.vtk", # pillow, bread
+    basePosition=[0,0,1],  # Position on top of the scoop
+    baseOrientation=p.getQuaternionFromEuler([math.pi/2, 0, math.pi/2]),
+    scale=.5,
+    mass=0.005,
     useNeoHookean=1,
-    NeoHookeanMu=.5,
-    # NeoHookeanLambda=1,
-    NeoHookeanDamping=.1,
+    NeoHookeanMu=1.5,
+    NeoHookeanLambda=1,
+    NeoHookeanDamping=0.1,
     useSelfCollision=1,
     frictionCoeff=0.5,
     collisionMargin=0.001
 )
-p.changeVisualShape(pillow, -1, rgbaColor=[1, 1, 1, 1], textureUniqueId=tex, flags=0)
+p.changeVisualShape(object_id, -1, rgbaColor=[1, 1, 1, 1], textureUniqueId=tex, flags=0)
 
-# Create a heavy box to fix the insole at the other end
+# Load the pillow soft body
+# object_id = p.loadSoftBody(
+#     "asset/pillow.vtk", # pillow, bread
+#     basePosition=[0,0, 1],  # Position on top of the scoop
+#     baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+#     scale=2.5,
+#     mass=mass_pillow,
+#     useNeoHookean=1,
+#     NeoHookeanMu=.5,
+#     # NeoHookeanLambda=1,
+#     NeoHookeanDamping=.1,
+#     useSelfCollision=1,
+#     frictionCoeff=0.0,
+#     collisionMargin=0.001
+# )
+# p.changeVisualShape(object_id, -1, rgbaColor=[1, 1, 1, 1], textureUniqueId=tex, flags=0)
+
+# Create a heavy box to fix the bread at the other end
 box = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.1, 0.8, 0.1])
 box_body = p.createMultiBody(
     baseMass=0,
     baseCollisionShapeIndex=box,
-    basePosition=[5, 2.5, 0.1],
+    basePosition=[5, 2.5, 0.05],
     baseOrientation=p.getQuaternionFromEuler([0, 0, 0])
 )
 
@@ -99,32 +98,44 @@ scoop_target_position = [1, 0, 1.2]  # Target position for the scoop to move alo
 
 # Run simulation
 i = 0
+dt = 1. / 240.
 while True:
     # Get current position and orientation of the scoop
     scoop_position, scoop_orientation = p.getBasePositionAndOrientation(scoop)
-
-    # # Move the scoop towards the target position using velocity control
-    # new_position = [
-    #     scoop_position[0] + scoop_velocity if scoop_position[0] < scoop_target_position[0] else scoop_target_position[0],
-    #     scoop_position[1],
-    #     scoop_position[2]
-    # ]
-    # p.resetBasePositionAndOrientation(scoop, new_position, scoop_orientation)
+    object_position, object_orientation = p.getBasePositionAndOrientation(object_id)
 
     # Apply force on scoop
     if i > 100 and i < 800:
         p.applyExternalForce(scoop, -1, [7, 0, 0], scoop_position, p.WORLD_FRAME)
     
     # Lift the scoop
-    if i > 800 and i < 1000:
+    if i > 800 and i < 1100:
         p.applyExternalForce(scoop, -1, [0, 0, 11], scoop_position, p.WORLD_FRAME)
 
     # Fix the scoop in the air
-    if i > 1000:
+    if i == 1100:
     #     p.applyExternalForce(scoop, -1, [0, 0, -g*(mass_pillow+mass_scoop)], scoop_position, p.WORLD_FRAME)
         p.changeDynamics(scoop, -1, mass=0)
         p.resetBaseVelocity(scoop, [0, 0, 0], [0, 0, 0])
 
+    if i > 1100:
+        # # FORCE on object
+        # p.applyExternalForce(object_id, -1, [random.uniform(-10000,10000) for _ in range(3)], object_position, p.WORLD_FRAME)
+
+        # VELOCITY
+        limit = 2000
+        rand_acc = [random.uniform(-limit, limit) for _ in range(3)]
+        total_acc = rand_acc
+        total_acc[2] = g
+        delta_vel = [a * dt for a in total_acc]
+        curr_vel = p.getBaseVelocity(object_id)[0]
+        new_vel = [v + dv for v, dv in zip(curr_vel, delta_vel)]
+        p.resetBaseVelocity(object_id, new_vel, [0, 0, 0])
+        print("object base position", object_position[2])
+        
+        # # Random FORCE on robot
+        # p.applyExternalForce(scoop, -1, [random.uniform(-10000,10000) for _ in range(3)], scoop_position, p.WORLD_FRAME)
+
     p.stepSimulation()
-    time.sleep(2. / 240.)
+    time.sleep(1. / 240.)
     i += 1
