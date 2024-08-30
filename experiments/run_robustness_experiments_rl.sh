f3e5565075fe18ef37fed9716acaed9c8346d731
YvesDong <602522935@qq.com>
2024-08-30 22:10:12 +0200

    dlr tip curvature added

--- experiments/run_robustness_experiments_rl.sh
+++ experiments/run_robustness_experiments_rl.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# panda
+# vpush
 # Define the values for reward_weights[4] and reward_weights[5]
 robustness_values=(true false)
 
 # Define the random seeds
-random_seeds=(1)
+random_seeds=(5 6)
 
 # Define perturbation values
-# perturbs=(true false)
-perturbs=(true)
+perturbs=(true false)
 
 # Define total timesteps
-total_timesteps=2000000  # 2e6
+total_timesteps=5000000  # 5e6
 
 # Function to run the command in a new VSCode terminal
+# reward weights original: 5.0 1.0 1.0 1.0 100.0 0.0 0.0 0.0 
+# reward weights ppo30-33: 0.2 0.2 0.2 1.0 100.0 0.0 0.0 0.0
+# poor: 0.3 0.3 0.3 3.0 100.0 0.0 0.0 0.0
 run_in_vscode_terminal() {
   local robustness=$1
   local seed=$2
   local perturb=$3
 
   cmd="python3 experiments/train.py \
-        --env_id panda \
+        --env_id vpush \
         --algo ppo \
         --using_robustness_reward $robustness \
         --render_mode rgb_array \
         --n_envs 1 \
-        --reward_weights 1.0 0.01 1.0 1.0 100.0 0.0 0.0 0.0 \
+        --reward_weights 5.0 1.0 1.0 1.0 100.0 0.0 0.0 0.0 \
         --random_seed $seed \
         --device cuda \
         --perturb $perturb "
@@ -42,6 +44,7 @@ for seed in "${random_seeds[@]}"; do
       run_in_vscode_terminal "$robustness" "$seed" "$perturb"
       sleep 60 # sec
     done
+  sleep 18000 # 5 hours
   done
 done
 
@@ -49,38 +52,35 @@ echo "All training scripts have been launched."
 
 
 
+# # !/bin/bash
 
-# #!/bin/bash
-
-# # vpush
+# # panda
 # # Define the values for reward_weights[4] and reward_weights[5]
 # robustness_values=(true false)
 
 # # Define the random seeds
-# random_seeds=(1 2 3)
+# random_seeds=(1)
 
 # # Define perturbation values
-# perturbs=(true false)
+# # perturbs=(true false)
+# perturbs=(true)
 
 # # Define total timesteps
-# total_timesteps=5000000  # 5e6
+# total_timesteps=2000000  # 2e6
 
 # # Function to run the command in a new VSCode terminal
-# # reward weights original: 5.0 1.0 1.0 1.0 100.0 0.0 0.0 0.0 
-# # reward weights ppo30-33: 0.2 0.2 0.2 1.0 100.0 0.0 0.0 0.0
-# # poor: 0.3 0.3 0.3 3.0 100.0 0.0 0.0 0.0
 # run_in_vscode_terminal() {
 #   local robustness=$1
 #   local seed=$2
 #   local perturb=$3
 
 #   cmd="python3 experiments/train.py \
-#         --env_id vpush \
+#         --env_id panda \
 #         --algo ppo \
 #         --using_robustness_reward $robustness \
 #         --render_mode rgb_array \
 #         --n_envs 1 \
-#         --reward_weights 5.0 1.0 1.0 1.0 100.0 0.0 0.0 0.0 \
+#         --reward_weights 1.0 0.01 1.0 1.0 100.0 0.0 0.0 0.0 \
 #         --random_seed $seed \
 #         --device cuda \
 #         --perturb $perturb "
@@ -94,7 +94,7 @@ echo "All training scripts have been launched."
 #   for perturb in "${perturbs[@]}"; do
 #     for robustness in "${robustness_values[@]}"; do
 #       run_in_vscode_terminal "$robustness" "$seed" "$perturb"
-#       sleep 4800 # sec
+#       sleep 60 # sec
 #     done
 #   done
 # done
