4e25bf969a7e8e938351a9c41945137e9344832d
SailorBrandon <shaohang.han@outlook.com>
2024-09-01 18:32:08 +0200

    add gpu assignment

--- experiments/run_rl_algo_panda.sh
+++ experiments/run_rl_algo_panda.sh
@@ -5,7 +5,7 @@
 robustness_values=(true false)
 
 # Define the random seeds
-random_seeds=(0 1)
+random_seeds=(0 1 2 3 4)
 
 # Define perturbation values
 perturbs=(true)
@@ -15,6 +15,9 @@ algos=(ppo sac)
 # Define total timesteps
 total_timesteps=5000000
 
+# Define GPU IDs to use
+gpus=(0 1 2 3)  # Modify this list based on the available GPUs and your requirements
+
 # Function to run the command in a new VSCode terminal
 run_in_vscode_terminal() {
   local robustness=$1
@@ -22,8 +25,10 @@ run_in_vscode_terminal() {
   local perturb=$3
   local rl_algo=$4
   local wandb_group_name=$5
+  local gpu_id=$6  # Added GPU ID
 
-  python3 experiments/train.py \
+  # Set the CUDA_VISIBLE_DEVICES environment variable to assign the GPU
+  CUDA_VISIBLE_DEVICES=$gpu_id python3 experiments/train.py \
         --env_id panda \
         --wandb_group_name "$wandb_group_name" \
         --wandb_mode online \
@@ -38,6 +43,9 @@ run_in_vscode_terminal() {
         --random_seed $seed &
 }
 
+# Counter for cycling through GPUs
+gpu_counter=0
+
 # Loop through each combination of robustness, perturb, and random seeds
 for perturb in "${perturbs[@]}"; do
     for robustness in "${robustness_values[@]}"; do
@@ -45,7 +53,16 @@ for perturb in "${perturbs[@]}"; do
             wandb_group_name="${rl_algo}, robustness ${robustness}, perturb ${perturb}"
             for seed in "${random_seeds[@]}"; do
                 echo "Running training with using_robustness_reward=$robustness, random_seed=$seed, perturb=$perturb, algo=$rl_algo"
-                run_in_vscode_terminal "$robustness" "$seed" "$perturb" "$rl_algo" "$wandb_group_name"
+                
+                # Assign a GPU from the list
+                gpu_id=${gpus[$gpu_counter]}
+                
+                # Run the training command with the specified GPU
+                run_in_vscode_terminal "$robustness" "$seed" "$perturb" "$rl_algo" "$wandb_group_name" "$gpu_id"
+                
+                # Increment the GPU counter and cycle through available GPUs
+                gpu_counter=$(( (gpu_counter + 1) % ${#gpus[@]} ))
+                
                 sleep 1000
             done
         done
