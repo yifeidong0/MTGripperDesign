b2f830bb3c64c7c903b6b65601359d89ec34f6fe
YvesDong <602522935@qq.com>
2024-09-08 09:26:56 +0200

    perturb plot

--- experiments/perturb_test.sh
+++ experiments/perturb_test.sh
@@ -26,33 +26,33 @@ random_seeds=(42 43 44 45 46)
 num_episodes=50
 render_mode="rgb_array" # "human"
 
-# Iterate over environments
-for env in "catch" "vpush" "panda" "dlr"; do
-    # env_id=${env_ids[$env]}
+# # Iterate over environments
+# for env in "catch" "vpush" "panda" "dlr"; do
+#     # env_id=${env_ids[$env]}
 
-    # Iterate over perturbation levels
-    for perturb in ${perturb_levels[$env]}; do
-        csv_file_a="results/paper/perturb/${env}_perturb_${perturb}_combination_1_robust_1_perturb_1.csv"
-        csv_file_b="results/paper/perturb/${env}_perturb_${perturb}_combination_2_robust_0_perturb_1.csv"
-        csv_file_c="results/paper/perturb/${env}_perturb_${perturb}_combination_3_robust_1_perturb_0.csv"
-        csv_file_d="results/paper/perturb/${env}_perturb_${perturb}_combination_4_robust_0_perturb_0.csv"
+#     # Iterate over perturbation levels
+#     for perturb in ${perturb_levels[$env]}; do
+#         csv_file_a="results/paper/perturb/${env}_perturb_${perturb}_combination_1_robust_1_perturb_1.csv"
+#         csv_file_b="results/paper/perturb/${env}_perturb_${perturb}_combination_2_robust_0_perturb_1.csv"
+#         csv_file_c="results/paper/perturb/${env}_perturb_${perturb}_combination_3_robust_1_perturb_0.csv"
+#         csv_file_d="results/paper/perturb/${env}_perturb_${perturb}_combination_4_robust_0_perturb_0.csv"
         
-        # Iterate over 5 random seeds
-        for i in {1..5}; do
-            model_files=($(ls ${models[$env]}/$i/*.zip | sort))
-            random_seed=${random_seeds[$i-1]}
+#         # Iterate over 5 random seeds
+#         for i in {1..5}; do
+#             model_files=($(ls ${models[$env]}/$i/*.zip | sort))
+#             random_seed=${random_seeds[$i-1]}
 
-            # Run the 4 different combinations
-            python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[0]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_a" 
-            python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[1]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_b"
-            python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[2]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_c"
-            python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[3]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_d"
-        done
-    done
-done
+#             # Run the 4 different combinations
+#             python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[0]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_a" 
+#             python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[1]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_b"
+#             python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[2]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_c"
+#             python3 experiments/perturb_ablation_test.py --num_episodes "$num_episodes"  --render_mode  "$render_mode" --model_path "${model_files[3]}" --env_id "$env" --perturb_sigma "$perturb" --perturb 1 --random_seed "$random_seed" --output_file "$csv_file_d"
+#         done
+#     done
+# done
 
 # Meta-data generation (mean and std across 500 trajectories)
-output_meta="results/paper/perturb/meta_results.csv"
+output_meta="results/paper/perturb/0_meta_results.csv"
 echo "env,perturb_sigma,combination_id,mean_success_rate,std_success_rate" > "$output_meta"
 
 # Iterate through all environments and perturb levels to gather meta-data
