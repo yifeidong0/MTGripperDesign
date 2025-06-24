6973b2e08ac13dedc8fad84a2d6e5e0816ec6b0e
SailorBrandon <shaohang.han@outlook.com>
2025-06-24 11:27:59 +0300

    tuned params

--- experiments/panda_RL.py
+++ experiments/panda_RL.py
@@ -25,27 +25,26 @@ if __name__ == "__main__":
     env = PandaEnvWrapper(env)
     
     if mode == "train":
-        # model = PPOReg(policy=DeepPolicy,
-        #             env=env,
-        #             verbose=1, 
-        #             tensorboard_log=f"data/runs/{env_id}/",
-        #             max_grad_norm=0.01,
-        #             clip_range=0.01,
-        #             n_steps=1024,
-        #             )
-        model = PPO(policy=DeepPolicy,
+        model = PPOReg(policy=DeepPolicy,
                     env=env,
-                    verbose=1, 
+                    verbose=1,
                     tensorboard_log=f"data/runs/{env_id}/",
                     max_grad_norm=0.01,
                     clip_range=0.01,
                     )
+        # model = PPO(policy=DeepPolicy,
+        #             env=env,
+        #             verbose=1,
+        #             tensorboard_log=f"data/runs/{env_id}/",
+        #             max_grad_norm=0.01,
+        #             clip_range=0.01,
+        #             )
         
         device = "cuda" if torch.cuda.is_available() else "cpu"
         pretrained_policy_path = "data/panda_bc_policy.pth"
         saved_variables = torch.load(pretrained_policy_path, map_location=device, weights_only=False)
         if isinstance(model, PPOReg):
-            model.set_pretrained_policy(saved_variables, log_std_init=-3.0, reg_coef=0.1)
+            model.set_pretrained_policy(saved_variables, log_std_init=-1.0, reg_coef=0.1)
         else:
             model.policy.load_state_dict(saved_variables['state_dict'])
         
@@ -65,7 +64,7 @@ if __name__ == "__main__":
     else:
         model = PPOReg.load(f"data/models/{env_id}_final_model.zip", env=env, policy=DeepPolicy)
         model.policy.eval()  # Set the policy to evaluation mode
-        n_episodes = 10
+        n_episodes = 1
         mean_reward, std_reward = evaluate_policy(model.policy, env, n_eval_episodes=n_episodes, deterministic=True)
         env.close()
         print(f"After fine-tuning, average reward over {n_episodes} episodes: {mean_reward}")
